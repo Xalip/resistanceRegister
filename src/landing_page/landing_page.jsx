e7c64ea5f0471f6fd58d1dc732b68e4b26a9ba07
doti1011 <doti1011@hs-karlsruhe.de>
2020-03-27 23:13:38 +0100

    added react-router-dom

--- src/landing_page/landing_page.jsx
+++ src/landing_page/landing_page.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import './landing_page.css';
-
+import React from "react";
+import "./landing_page.css";
 
 function LandingPage() {
-    return (
-        <div className="content">
-            <header>
-                <div className="title">esistanceRegister</div>
-            </header>
-            <div className="maingrid">
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="content">
+      <header>
+        <div className="title">esistanceRegister</div>
+      </header>
+      <div className="maingrid"></div>
+    </div>
+  );
 }
-
 
 export default LandingPage;
