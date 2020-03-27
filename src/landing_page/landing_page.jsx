75e2a32e5a1f4e3a0d0bc8c122bc7a7109b99150
doti1011 <doti1011@hs-karlsruhe.de>
2020-03-27 21:48:26 +0100

    Landing Page completed

--- src/landing_page/landing_page.jsx
+++ src/landing_page/landing_page.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './landing_page.css';
 
+
 function LandingPage() {
     return (
-        <div>
-            <div className="content">
-
+        <div className="content">
+            <header>
+                <div className="title">esistanceRegister</div>
+            </header>
+            <div className="maingrid">
+            
             </div>
         </div>
     );
