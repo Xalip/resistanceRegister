2427b4c130e412b2ee3c43b8acba3ff505df0527
soccermax <max.grunfelder@gmail.com>
2020-03-27 18:31:49 +0100

    connect firebase to react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+//TODO: move to env
+const firebaseConfig = {
+  apiKey: "AIzaSyCfPIosSU5eg7X5-xF-X-2llyDvam9SKlg",
+  authDomain: "resistanceregister.firebaseapp.com",
+  databaseURL: "https://resistanceregister.firebaseio.com",
+  projectId: "resistanceregister",
+  storageBucket: "resistanceregister.appspot.com",
+  messagingSenderId: "87921927745",
+  appId: "1:87921927745:web:d13d1e18189dceb271c8c0"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
