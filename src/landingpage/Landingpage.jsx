21c35f615ef227e578b9a98e3e4266418ca8cd25
doti1011 <doti1011@hs-karlsruhe.de>
2020-03-28 21:26:52 +0100

    added map search

--- src/landingpage/Landingpage.jsx
+++ src/landingpage/Landingpage.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import "./Landingpage.css";
 import { Link } from "react-router-dom";
 
 function LandingPage() {
+  let history = useHistory();
   return (
     <div className="content">
       <header>
         <div className="title">esistanceRegister</div>
       </header>
       <div className="maingrid">
-        Hier folgen Informationen zu diesem Projekt. Eine Verzweigung auf die
-        Übersichtsseite findet Ihr hier: <Link to="/overview">Overview</Link>
+        Via ResistanceRegister you can help in the fight against Covid-19 by
+        sharing your resistance status. Therefore just register on our portal
+        and furthermore keep up track on your cities total status.
+        <Button
+          className="startNow"
+          onClick={() => {
+            history.push("/signUp");
+          }}
+        >
+          Start Now
+        </Button>
+        <Link to="/overview">Overview</Link>
       </div>
     </div>
   );
