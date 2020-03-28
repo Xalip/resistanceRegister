7e7a5390dab31747c9a005754faab91fe6249eb4
Thirsty Camel <thirsty.camel43@gmail.com>
2020-03-28 11:02:53 +0100

    intermediate signup

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 require("dotenv").config();
-
 const express = require('express');
 const app = express();
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require('cors');
+
+//Router files
+const userRouter = require("./router/user.js");
 
 const { loadFirebaseCrediantials } = require("./helper");
 
@@ -16,24 +18,6 @@ app.use(cors({ origin: true }));
 app.use('/', express.static('public'));
 app.use(require("body-parser").json());
 
-app.post('/createUser', async (req, res) => {
-    const userData = req.body;
-    const response = await admin
-        .firestore()
-        .collection("users")
-        .add({
-            firstname: userData.givenName,
-            lastname: userData.familyName,
-            email: userData.email,
-            password: userData.password
-        })
-    res.send("User successfully created");
-    console.log(response);
-});
-
-app.get('/downloadxml', function (req, res) {
-    res.send("test");
-});
-
+app.use("/user", userRouter);
 
 exports.api = functions.https.onRequest(app);
---
+++ src/helper/SignUp.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import axios from 'axios';
+
+async function checkGoogleUserExists() {
+    const response = axios.get(``)
+}
+
+
+
+export default {
+    checkGoogleUserExists
+}
+
+
+
