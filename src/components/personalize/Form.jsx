933bb7ff416b6bc4622eb0580c9c87975745c067
Manuel Hieke <manuelhieke@Manuels-MBP.fritz.box>
2020-03-28 21:54:13 +0100

    restructure data for personalize formular resolve warnings

--- src/components/personalize/Form.jsx
+++ src/components/personalize/Form.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Form extends React.Component {
 
     constructor(props) {
         super(props);
-
-        // TODO: flag-binding not working yet
-        // TODO: mutation of value in dynamic fields not possible -> probalby bindin stuff
-        // TODO: occupation selection -> not selected required value should be cleared
 
         this.state = {
             firstName: "Hans",
@@ -19,23 +14,57 @@ class Form extends React.Component {
             zip: "1000",
             city: "Zürich",
             occupation: "student",
-            school: "Gymnasium",
-            job: "Krankenpfleger",
-            company: "Krankenhaus Altstetten",
-            occupationDescription: "Arbeitssuchend",
+            occupationData: {
+                school: "Gymnasium",
+            },
             iWannaHelp: true,
             publishData: true
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleSelectOccupation = this.handleSelectOccupation.bind(this);
-
+        this.handleOccupationChange = this.handleOccupationChange.bind(this);
+        this.changeOccupationData = this.changeOccupationData.bind(this);
     }
 
     handleChange(event) {
-        this.state[event.target.id] = event.target.value;
-        this.setState(this.state);
+        const { id, value } = event.target;
+        this.setState({ ...this.state, [id]: value });
+    }
+
+    handleCheckboxChange(event) {
+        const { id, checked } = event.target;
+        this.setState({ ...this.state, [id]: checked });
+    }
+
+    handleOccupationChange(event) {
+        const occupationValue = event.target.value;
+        this.setState({occupation: occupationValue})
+        if (occupationValue === "student") {
+            this.setState({
+                occupationData: {
+                    school: ""
+                }
+            });
+        } else if (occupationValue === "self-employed" || occupationValue === "employed") {
+            this.setState({
+                occupationData: {
+                    job: "",
+                    company: ""
+                }
+            })
+        } else if (occupationValue === "other") {
+            this.setState({
+                occupationData: {
+                    occupationDescription: ""
+                }
+            });
+        }
+    }
+
+    changeOccupationData({target}) {
+        this.setState({occupationData: {...this.state.occupationData, [target.id]: target.value}});
     }
 
     handleSubmit(event) {
@@ -74,55 +103,55 @@ class Form extends React.Component {
     getName() {
         return <div className="row">
             <div className="col">
-                <label for="firstName">Vorname</label>
-                <input type="text" class="form-control" id="firstName" value={this.state.firstName} onChange={this.handleChange} required></input>
+                <label htmlFor="firstName">Vorname</label>
+                <input type="text" className="form-control" id="firstName" value={this.state.firstName} onChange={this.handleChange} required></input>
             </div>
             <div className="col">
-                <label for="lastName">Nachname</label>
-                <input type="text" class="form-control" id="lastName" value={this.state.lastName} onChange={this.handleChange} required></input>
+                <label htmlFor="lastName">Nachname</label>
+                <input type="text" className="form-control" id="lastName" value={this.state.lastName} onChange={this.handleChange} required></input>
             </div>
         </div>
     }
 
     getBirthDay() {
         return <div>
-            <label for="birthday">Geburtsdatum</label>
-            <input type="date" class="form-control" id="birthday" required value={this.state.dateOfBirth} onChange={this.handleChange}></input>
+            <label htmlFor="birthday">Geburtsdatum</label>
+            <input type="date" className="form-control" id="birthday" required value={this.state.dateOfBirth} onChange={this.handleChange}></input>
         </div>
     }
 
     getEmail() {
         return <div>
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" readOnly value={this.state.email}></input>
+            <label htmlFor="email">Email</label>
+            <input type="email" className="form-control" id="email" readOnly value={this.state.email}></input>
         </div>
     }
 
     getPhone() {
         return <div>
-            <label for="phone">Telefon <span class="text-muted">(Optional)</span></label>
-            <input type="tel" class="form-control" id="phone" value={this.state.phone} onChange={this.handleChange}></input>
+            <label htmlFor="phone">Telefon <span className="text-muted">(Optional)</span></label>
+            <input type="tel" className="form-control" id="phone" value={this.state.phone} onChange={this.handleChange}></input>
         </div>
     }
 
     getZipCity() {
         return <div className="row">
             <div className="col col-md-4">
-                <label for="zip">PLZ</label>
-                <input type="text" class="form-control" id="zip" required value={this.state.zip} onChange={this.handleChange}></input>
+                <label htmlFor="zip">PLZ</label>
+                <input type="text" className="form-control" id="zip" required value={this.state.zip} onChange={this.handleChange}></input>
             </div>
             <div className="col">
-                <label for="city">Ort</label>
-                <input type="text" class="form-control" id="city" required value={this.state.city} onChange={this.handleChange}></input>
+                <label htmlFor="city">Ort</label>
+                <input type="text" className="form-control" id="city" required value={this.state.city} onChange={this.handleChange}></input>
             </div>
         </div>
     }
 
     getOccupationField() {
         return <div>
-            <div class="form-group">
-                <label for="occupation">Tätigkeit</label>
-                <select class="form-control" id="occupation" value={this.state.occupation} onChange={this.handleSelectOccupation}>
+            <div className="form-group">
+                <label htmlFor="occupation">Tätigkeit</label>
+                <select className="form-control" id="occupation" value={this.state.occupation} onChange={this.handleOccupationChange}>
                     <option value="student">Schüler(in) / Student(in)</option>
                     <option value="employed">Berufstätig</option>
                     <option value="self-employed">Selbständig</option>
@@ -130,54 +159,47 @@ class Form extends React.Component {
                     <option value="other">Andere</option>
                 </select>
             </div>
-            <div id="occupationContainer" >{this.getStudentPanel()}</div>
+            <div id="occupationContainer" >{this.getOccupationContainer(this.state.occupation)}</div>
         </div>
     }
 
-    handleSelectOccupation(event) {
-        let selected = event.target.value;
-        this.state.occupation = selected;
-        this.setState(this.state);
+    getOccupationContainer(occupation) {
         let dynamicPanel = "";
-        if (selected == "student") {
+        if (occupation === "student") {
             dynamicPanel = this.getStudentPanel();
-        } else if (selected == "self-employed" || selected == "employed") {
+        } else if (occupation === "self-employed" || occupation === "employed") {
             dynamicPanel = this.getEmployedPanel();
-        } else if (selected == "other") {
+        } else if (occupation === "other") {
             dynamicPanel = this.getOtherPanel();
         }
-        ReactDOM.render(
-            dynamicPanel,
-            document.getElementById('occupationContainer')
-        );
+        return dynamicPanel;
     }
 
     getStudentPanel() {
         return <div className="form-group">
-            <label for="school">Schule</label>
-            <input type="text" class="form-control" id="school" required value={this.state.school} onChange={this.handleChange}></input>
+            <label htmlFor="school">Schule</label>
+            <input type="text" className="form-control" id="school" required value={this.state.occupationData.school} onChange={this.changeOccupationData}></input>
         </div>
     }
 
     getEmployedPanel() {
         return <div className="form-group"><div className="form-group" >
-            <label for="job">Beruf</label>
-            <input type="text" class="form-control" id="job" required value={this.state.job} onChange={this.handleChange}></input>
+            <label htmlFor="job">Beruf</label>
+            <input type="text" className="form-control" id="job" required value={this.state.occupationData.job} onChange={this.changeOccupationData}></input>
         </div>
             <div className="form-group">
-                <label for="compnay">Firma</label>
-                <input type="text" class="form-control" id="company" required value={this.state.company} onChange={this.handleChange}></input>
+                <label htmlFor="company">Firma</label>
+                <input type="text" className="form-control" id="company" required value={this.state.occupationData.company} onChange={this.changeOccupationData}></input>
             </div>
         </div>
     }
 
     getOtherPanel() {
         return <div className="form-group">
-            <label for="occupationDescription">Bezeichnung</label>
-            <input type="text" class="form-control" id="occupationDescription" required value={this.state.occupationDescription} onChange={this.handleChange}></input>
+            <label htmlFor="occupationDescription">Bezeichnung</label>
+            <input type="text" className="form-control" id="occupationDescription" required value={this.state.occupationData.description} onChange={this.changeOccupationData}></input>
         </div>
     }
-
 
     getApprovalFlags() {
         return [
@@ -187,16 +209,16 @@ class Form extends React.Component {
     }
 
     getIWannaHelp() {
-        return <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="iWannaHelp" required value={this.state.iWannaHelp} onChange={this.handleChange} />
-            <label class="form-check-label" for="iWannaHelp">Ich möchte helfen</label>
+        return <div className="form-check">
+            <input className="form-check-input" type="checkbox" id="iWannaHelp" checked={this.state.iWannaHelp} onChange={this.handleCheckboxChange} />
+            <label className="form-check-label" htmlFor="iWannaHelp">Ich möchte kontaktiert werden</label>
         </div>
     }
 
     getPublishMyData() {
-        return <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="publishData" value={this.state.publishData} onChange={this.handleChange} />
-            <label class="form-check-label" for="publishData">Daten anonymisiert veröffentlichen</label>
+        return <div className="form-check">
+            <input className="form-check-input" type="checkbox" id="publishData" checked={this.state.publishData} onChange={this.handleCheckboxChange} />
+            <label className="form-check-label" htmlFor="publishData">Daten anonymisiert veröffentlichen</label>
         </div>
     }
 
