579fcd9e7ce1086e5f2fce82e8a0bba96b8bc452
Thirsty Camel <thirsty.camel43@gmail.com>
2020-03-28 10:16:41 +0100

    removed keys to env file

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import * as firebase from 'firebase';
 
 //TODO: move to env
 const firebaseConfig = {
-  apiKey: "AIzaSyCfPIosSU5eg7X5-xF-X-2llyDvam9SKlg",
-  authDomain: "resistanceregister.firebaseapp.com",
-  databaseURL: "https://resistanceregister.firebaseio.com",
-  projectId: "resistanceregister",
-  storageBucket: "resistanceregister.appspot.com",
-  messagingSenderId: "87921927745",
-  appId: "1:87921927745:web:d13d1e18189dceb271c8c0"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
