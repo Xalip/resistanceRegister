249cec67c070eb0047f13b4b9864de1b2ca3c78c
mperator <develop@mperia.de>
2020-03-29 11:03:14 +0200

    Add upload function.

--- src/components/testupload/TestUpload.jsx
+++ src/components/testupload/TestUpload.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import "./TestUpload.css"
+import axios from 'axios';
 
 class TestUpload extends Component {
     constructor(props) {
@@ -7,12 +8,19 @@ class TestUpload extends Component {
 
         this.state = {
             image: null,
+            imageContentType: null,
             imageLoadingProgressEnabled: false,
             resultOptions: [
-                { display: "Please select", value: 0 },
-                { display: "Not Resistant", value: 1 },
-                { display: "Resistant", value: 2 }],
-            result: 0
+                { display: "Please select", value: 'NONE' },
+                { display: "Test has been positive.", value: "POSITIVE" },
+                { display: "Test has been negative.", value: "NEGATIVE" },
+                { display: "User has not been tested.", value: "NOTBEENTESTED" },
+                { display: "User has been recovered.", value: "RECOVERED" },
+            ],
+            result: 'NONE',
+            userId: '3JA3ubus7gAj2IuGZGQE',
+            isUploading: false,
+            errorMessage: ''
         }
 
         this.handleFileChange = this.handleFileChange.bind(this);
@@ -39,7 +47,7 @@ class TestUpload extends Component {
                                     <img src={this.state.image} style={{ height: "500px" }} className="img-fluid" alt="" /> :
 
                                     (this.state.imageLoadingProgressEnabled &&
-                                        <div class="text-center">
+                                        <div className="text-center">
                                             <div className="spinner-border" role="status">
                                                 <span className="sr-only">Loading...</span>
                                             </div>
@@ -62,9 +70,22 @@ class TestUpload extends Component {
                         </div>
 
                         <div className="form-group">
-                            <button className="btn btn-primary float-right" type="submit" disabled={!this.isSaveEnabled()}>Speichern</button>
+                            <button className="btn btn-primary float-right" type="submit" disabled={!this.isSaveEnabled()}>
+                                {this.state.isUploading ?
+                                    <Fragment>
+                                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                    Loading... </Fragment> :
+                                    "Speichern"
+                                }
+                            </button>
                         </div>
+
+                        {this.state.errorMessage &&
+                            <span className="text-danger">
+                                {this.state.errorMessage}
+                            </span>}
                     </form>
+
                 </div>
             </div>
         );
@@ -72,13 +93,34 @@ class TestUpload extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        
-        // this.state.image = base64 blob
-        // this.state.result = id of result 
+
+        const options = {
+            headers: {
+                'content-type': this.state.imageContentType,
+                'testResult': this.state.result
+            }
+        };
+
+        this.setState({ isUploading: true});
+
+
+        const url = `${process.env.NODE_ENV === "production" ? process.env.REACT_APP_BASE_API_DEPLOY_URL : process.env.REACT_APP_BASE_API_LOCAL_URL}/user/uploadResult?userID=${this.state.userId}`;
+
+        console.log(url);
+
+        axios.post(url, this.state.image, options)
+            .then(e => {
+                this.props.history.push('/overview');
+            })
+            .catch(e => {
+                this.setState({ 
+                    isUploading: false,
+                    errorMessage: "Could not upload data, please try again." });
+            });
     }
 
     handleSelectionChange(e) {
-        this.setState({ result : e.target.value});
+        this.setState({ result: e.target.value });
     }
 
     handleFileChange(e) {
@@ -88,10 +130,10 @@ class TestUpload extends Component {
             this.setState({ imageLoadingProgressEnabled: true })
 
             reader.onload = (a) => {
-                console.log(a.target.result)
-
+                let mimeType2 = a.target.result.match(/[^:/]\w+(?=;|,)/)[0];
                 this.setState({
                     image: a.target.result,
+                    imageContentType: `image/${mimeType2}`,
                     imageLoadingProgressEnabled: false
                 });
             };
@@ -101,7 +143,7 @@ class TestUpload extends Component {
     }
 
     isSaveEnabled() {
-        return this.state.result && this.state.result > 0 && this.state.image;
+        return this.state.result && this.state.result !== 'NONE' && this.state.image;
     }
 }
 
